a2da4fa8a0880624d5a60b9a8a134dfdbefcac1c
Guillaume Roboam <guillaume_roboam@hotmail.fr>
2025-09-09 23:10:14 +0200

    refactor: streamline deployment process by removing script and integrating commands directly in deploy.yml

--- src/scripts/deploy-to-pi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-SSH_USER="$1"
-SSH_HOST="$2"
-
-scp -o StrictHostKeyChecking=no ./* "$SSH_USER@$SSH_HOST:/var/www/html/"
-ssh -o StrictHostKeyChecking=no "$SSH_USER@$SSH_HOST" "sudo systemctl restart nginx"
